8b2989629d9fd5d21dfdcbfca6d695c98b0cbf9d
hhd389 <huahoangduy.389@gmail.com>
2020-10-17 12:08:10 +0700

    add nyrequest file

--- js/login.js
+++ js/login.js
@@ -9,3 +9,25 @@ input.forEach(item => {
         }
     });
 });
+// forgot password
+var click = document.querySelector("#forgot");
+var feedback = document.querySelector(".feedback-container");
+click.addEventListener('click',(e) => {
+    e.preventDefault();
+    feedback.classList.toggle("actives");
+});
+var fb = document.querySelector(".feedback-fixed");
+feedback.addEventListener('click',function(e){
+    var div = e.target;
+    if(div.classList.contains('feedback-container'))
+    {
+        feedback.classList.remove("actives");
+    }
+});
+var fb_close = document.querySelector('.close');
+if(fb_close){
+    fb_close.addEventListener('click',function(e){
+        e.preventDefault();
+        feedback.classList.remove("actives");
+    });
+}
